f56095d36a10520eac192e6c6a3df77f45ed5f06
chen.levin <chen.levin@ironsrc.com>
2022-06-27 12:33:16 +0300

    first commit

---
+++ src/components/button/Button.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+import {computed} from 'vue';
+
+const props = withDefaults(defineProps<{
+    size?: 'small' | 'medium' | 'big'
+}>(),
+    {
+        size: 'medium'
+    });
+
+
+const cls = computed(() => props.size);
+
+</script>
+
+<template>
+
+    <button :class="[cls]">
+        <slot />
+    </button>
+
+</template>
+
+<style scoped>
+.small {
+    width: 25px;
+    height: 25px;
+}
+
+.medium {
+    width: 40px;
+    height: 40px;
+}
+
+.large {
+    width: 50px;
+    height: 50px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "VueLibSandbox",
+      fileName: (format) => `vue-lib-sandbox.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+})
+
+
